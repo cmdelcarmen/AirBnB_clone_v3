f9e573201301d2b18ab8395fa2e74e59945d7fb2
Caroline Del Carmen <delcarmencaroline@gmail.com>
2021-09-19 20:43:33 +0000

    added error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-    API for HBNB
+API for HBNB
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 
@@ -14,9 +14,17 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown(exception):
     """
-        Closes current SQLAlchemy session.
+    Closes current SQLAlchemy session.
     """
     storage.close()
+
+@app.errorhandler(404)
+def handle_not_found(err=None):
+    """
+    Handler for 404 errors that returns
+    a JSON-formatted 404 status code response
+    """
+    return (jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(getenv('HBNB_API_HOST', '0.0.0.0'),
