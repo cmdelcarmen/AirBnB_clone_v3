a7ffab5f9c97e549a92150c6a2272b32444e7907
Caroline Del Carmen <77739870+cmdelcarmen@users.noreply.github.com>
2021-09-19 23:41:55 -0400

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-'''
-View for state objects
-'''
 
-
-
+import json
+from werkzeug.exceptions import NotFound
+from flask import request, jsonify, make_response
 from models import storage
 from models.state import State
-from flask import jsonify, Flask, request, abort
-import os
 from api.v1.views import app_views
 
 
@@ -26,33 +22,51 @@ def all_states():
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state(state_id=None):
-    '''
-    Retrieve a State object
-    '''
+def state_retrieve(state_id):
+    state = storage.get(State, state_id)
 
-    stateObj = storage.get(State, state_id)
-    if (stateObj):
-        return (jsonify(stateObj.to_dict()))
-    else:
-        abort(404)
+    if state is None:
+        raise NotFound
+    return (jsonify(state.to_dict()))
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-def del_state(state_id=None):
-    '''
-    Delete a state object
-    '''
+def state_delete(state_id):
+    state = storage.get(State, state_id)
 
-    stateObj = storage.get(State, state_id)
-    if (stateObj):
-        storage.delete(stateObj)
-        storage.save()
-        return (jsonify({}), 200)
+    if state is None:
+        raise NotFound
 
-    else:
-        abort(404)
+    state.delete()
+    storage.save()
+
+    return ({}, 200)
+
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+def state_create():
+    if not request.json:
+        return make_response('Not a JSON', 400)
+
+    if 'name' not in request.get_json().keys():
+        return make_response('Missing name', 400)
+
+    state = State(**request.get_json())
+    state.save()
+    return (jsonify(state.to_dict()), 201)
 
 
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def state_update(state_id=None):
+    state = storage.get(State, state_id)
 
+    if state is None:
+        raise NotFound
 
+    if not request.json:
+        return make_response('Not a JSON', 400)
+
+    for key, value in request.get_json().items():
+        setattr(state, key, value)
+
+    state.save()
+    return (jsonify(state.to_dict()), 200)
