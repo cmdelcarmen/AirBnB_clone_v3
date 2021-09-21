c714b0915eb3fe2985db248f65637c73bcf937a0
Caroline Del Carmen <77739870+cmdelcarmen@users.noreply.github.com>
2021-09-20 22:26:18 -0400

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """States views"""
+from flask import jsonify, make_response, abort, request
+from api.v1.views import app_views
 from models import storage
 from models.state import State
 from models.city import City
-from flask import jsonify, make_response, abort, request
-from api.v1.views import app_views
 
 
 @app_views.route('/states/<id>/cities',
@@ -25,27 +25,27 @@ def view_cities_of_state(id):
 
     if request.method == 'POST':
         # Get the attributes from the request
-        d = request.get_json()
+        data = request.get_json()
 
-        if isinstance(d, dict):
+        if isinstance(data, dict):
             pass
         else:
             return jsonify({"error": "Not a JSON"}), 400
 
-        if 'name' not in d.keys():
+        if 'name' not in data.keys():
             return jsonify({'error': 'Missing name'}), 400
 
-        if 'id' in d.keys():
-            d.pop("id")
-        if 'created_at' in d.keys():
-            d.pop("created_at")
-        if 'updated_at' in d.keys():
-            d.pop("updated_at")
+        if 'id' in data.keys():
+            data.pop("id")
+        if 'created_at' in data.keys():
+            data.pop("created_at")
+        if 'updated_at' in data.keys():
+            data.pop("updated_at")
 
-        d.update({"state_id": id})
+        data.update({"state_id": id})
 
         # Create the object
-        obj = City(**d)
+        obj = City(**data)
 
         # Save the object in storage
         storage.new(obj)
@@ -71,20 +71,20 @@ def view_city_id(id):
         return jsonify({}), 200
 
     if request.method == 'PUT':
-        d = request.get_json()
+        data = request.get_json()
         if isinstance(data, dict):
             pass
         else:
             return jsonify({"error": "Not a JSON"}), 400
 
-        if 'id' in d.keys():
-            d.pop("id")
-        if 'created_at' in d.keys():
-            d.pop("created_at")
-        if 'updated_at' in d.keys():
-            d.pop("updated_at")
+        if 'id' in data.keys():
+            data.pop("id")
+        if 'created_at' in data.keys():
+            data.pop("created_at")
+        if 'updated_at' in data.keys():
+            data.pop("updated_at")
 
-        for key, value in d.items():
+        for key, value in data.items():
             setattr(city, key, value)
 
         storage.save()
